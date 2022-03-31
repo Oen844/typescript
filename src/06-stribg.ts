1031893e5f2fb08a31ee6547d26ba4e534d8cf64
Pau Egea <papau84@gmail.com>
2022-03-31 19:38:32 +0200

    String

---
+++ src/06-stribg.ts
@@ -0,0 +1,16 @@
+(()=>{
+  let productTitle = 'My amazing product';
+
+  productTitle = 'My amazing has changed';
+  console.log('productTitle', productTitle);
+
+  const productDescription = "This is a product description";
+  console.log('productDescription', productDescription);
+
+  const summary = `
+  title: ${productTitle}
+  description: ${productDescription},
+  Summary ${productTitle}`;
+
+
+})()
