6dd98cb3409bb5c11d9a35f7826ff1ac4acff645
Pau Egea <papau84@gmail.com>
2022-03-31 19:21:56 +0200

    numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,35 @@
+(()=>{
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('productPrice', productPrice);
+
+  let customerAge: number = 30;
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge);
+
+  let productoInStock: number = 10;
+  console.log('productoInStock', productoInStock);
+
+  if(productoInStock<10){
+    console.log('productoInStock', productoInStock);
+  }
+
+  let discount = parseInt('ssdasdad');
+  console.log('discount', discount);
+
+  if(discount <= 200){
+    console.log('apply discount', discount);
+  }else{
+    console.log('no discount', discount);
+  }
+
+  let hex = 0xfff;
+  console.log('hex', hex);
+
+  let binary = 0b1010101010101010101010101010101;
+  console.log('binary', binary);
+
+  const numerito : Number = 123;
+  console.log('numerito', numerito);
+
+})();
