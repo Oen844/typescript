3665aa8d513a5a78d535be20092dfc766b7cf993
Pau Egea <papau84@gmail.com>
2022-03-31 15:52:05 +0200

    TSConfig.json

---
+++ src/02-demo2.ts
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5];
+console.log("Hello World");
+console.log(numbers);
+
+//create funtion say hello
+function sayHello(name: string) {
+
+  console.log("Hello " + name);
+}
+const name2:string = "John";
+
+sayHello(name2);
